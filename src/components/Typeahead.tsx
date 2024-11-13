3425077d78b10a1d92a58bd1ba9bece4fd1b4189
Oliver Daniel <oliverdaniel00@gmail.com>
2024-11-13 09:48:10 -0500

    create Typeahead component and PoC for autosuggesting cities

---
+++ src/components/Typeahead.tsx
@@ -0,0 +1,56 @@
+import { useRef, useState } from "react";
+import styles from "@/styles/components/typeahead.module.scss";
+
+export type RenderContext = {
+  ref: React.RefObject<HTMLInputElement>;
+  dispatch: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+type Props<T> = {
+  values: T[];
+  renderItem: (item: T, i: number, ctx: RenderContext) => JSX.Element;
+  filterItemKey?: keyof T;
+};
+
+export const Typeahead = <ValueType,>({
+  values,
+  renderItem,
+  filterItemKey,
+}: Props<ValueType>) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [inputHasFocus, setInputFocus] = useState(false);
+  const [search, setSearch] = useState("");
+
+  return (
+    <div className={styles.typeahead}>
+      <input
+        ref={inputRef}
+        onFocusCapture={() => setInputFocus(true)}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <details className="dropdown" open={inputHasFocus}>
+        <summary aria-haspopup="listbox" style={{ display: "none" }} />
+        <ul role="listbox">
+          {values.length === 0 ? (
+            <li aria-busy="true" />
+          ) : (
+            values
+              .filter(
+                (item) =>
+                  !filterItemKey ||
+                  (item[filterItemKey] as string)
+                    .toLocaleLowerCase()
+                    .includes(search.toLocaleLowerCase())
+              )
+              .map((item, i) =>
+                renderItem(item, i, {
+                  ref: inputRef,
+                  dispatch: setInputFocus,
+                })
+              )
+          )}
+        </ul>
+      </details>
+    </div>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,22 @@
-import { CurrentWeatherResponse } from "@/types/open-weather";
+import {
+  Typeahead,
+  RenderContext as TypeaheadRenderContext,
+} from "@/components/Typeahead";
+import { CityGeocode, CurrentWeatherResponse } from "@/types/open-weather";
+import { fetchCitySuggestions } from "@/util/fetch-cities";
 import { fetchWeather } from "@/util/fetch-weather";
 import { useEffect, useState } from "react";
 
 export default function Home() {
   const [weather, setWeather] = useState({} as CurrentWeatherResponse);
+  const [cities, setCities] = useState([] as CityGeocode[]);
+
+  useEffect(() => {
+    (async () => {
+      const citySuggestions = await fetchCitySuggestions();
+      setCities(citySuggestions);
+    })();
+  }, []);
 
   useEffect(() => {
     (async () => {
@@ -11,10 +24,34 @@ export default function Home() {
       setWeather(weatherResult);
     })();
   }, []);
+
+  const onItemSelect =
+    (city: CityGeocode, { ref: inputRef, dispatch }: TypeaheadRenderContext) =>
+    () => {
+      const label = `${city.name}, ${city.country}`;
+      if (inputRef.current) inputRef.current.value = label;
+      dispatch(false);
+    };
   return (
     <>
       <div>
         <pre>{JSON.stringify(weather, undefined, 2)}</pre>
+
+        <Typeahead
+          values={cities}
+          filterItemKey="name"
+          renderItem={(city, i, ctx) => (
+            <li key={`${city}-${i}`}>
+              <a
+                href="#"
+                onClick={onItemSelect(city, ctx)}
+                onSelect={onItemSelect(city, ctx)}
+              >
+                {city.name}, {city.country}
+              </a>
+            </li>
+          )}
+        />
       </div>
     </>
   );
